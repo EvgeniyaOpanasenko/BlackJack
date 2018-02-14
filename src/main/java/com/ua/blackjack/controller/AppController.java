5e37987b82f8377bebefc6e9e6e3a8d6e82738aa
evgeniya.opanasenko <evgeniya.opanasenko@outlook.com>
2018-02-14 16:34:06 +0200

    move index.html

--- src/main/java/com/ua/blackjack/controller/AppController.java
+++ src/main/java/com/ua/blackjack/controller/AppController.java
@@ -4,19 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Controller
-public class AppController {
+public class AppController extends WebMvcConfigurerAdapter {
 
-	@RequestMapping("/")
-	String home(ModelMap modal) {
-		modal.addAttribute("title","BlackJack");
-		return "index";
-	}
-
-	@RequestMapping("/partials/{page}")
-	String partialHandler(@PathVariable("page") final String page) {
-		return page;
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "index.html");
 	}
 
 }
